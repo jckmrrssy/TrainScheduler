6efa5f9f1e2568ee90653029ef0153f4965d555c
Jack Morrissey <j.morrissey.16@gmail.com>
2018-10-04 20:58:54 -0500

    first updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAN_AxTD6XqNqU2gOMSLgLAQbrhTitZDbY",
+        authDomain: "trainscheduler-12c22.firebaseapp.com",
+        databaseURL: "https://trainscheduler-12c22.firebaseio.com",
+        projectId: "trainscheduler-12c22",
+        storageBucket: "trainscheduler-12c22.appspot.com",
+        messagingSenderId: "408857104828"
+      };
+      
+      firebase.initializeApp(config);
+
+      let database = firebase.database();
+
+
+
+
+
+});
