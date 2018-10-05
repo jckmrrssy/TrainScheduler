43e6d86edfb45e4466af4cdc11abe5eec0032dfb
Jack Morrissey <j.morrissey.16@gmail.com>
2018-10-04 23:17:06 -0500

    added table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,31 @@ $(document).ready(function() {
       
       firebase.initializeApp(config);
 
-      let database = firebase.database();
+      var database = firebase.database();
 
+      var trainName = '';
+      var destination = '';
+      var startTime = '';
+      var frequency = 0;
+
+        // Button click capture
+      $("#submitIt").on("click", function(event) {
+        event.preventDefault();
+        trainName = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        startTime = $("#firstTrainInput").val().trim();
+        frequency = $("#frequencyInput").val().trim();
+
+        // Push values to Firebase
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            startTime: startTime,
+            frequency: frequency,
+        });
+
+
+      });
 
 
 
